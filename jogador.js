e9cfd94ec32c386fe94e4cb2dae28a44b4e17bea
gustavoq <Comercial@rede.tecmetro>
2025-09-03 16:24:07 -0300

    Alpha 1.3

--- jogador.js
+++ jogador.js
@@ -1,33 +1,4 @@
-// entrada.js - Versão Simplificada
 
-// Seleciona os elementos específicos desta página
-const welcomeMessage = document.getElementById('welcome-message');
-const logoutButton = document.getElementById('logout-button');
-
-// Esta é uma função que se executa sozinha assim que o script carrega
-(async () => {
-    // A função 'checkUserSession' vem do arquivo auth.js (que já foi carregado)
-    // Ela nos retorna o usuário logado (ou já nos redirecionou se não houver login)
-    const user = await checkUserSession();
-
-    // Se a função retornou um usuário, nós o usamos para popular a página
-    if (user) {
-        welcomeMessage.textContent = `Bem-vindo, ${user.email}!`;
-    }
-})(); // Os parênteses no final fazem a função rodar imediatamente
-
-// A lógica de logout continua aqui, pois o botão só existe nesta página
-logoutButton.addEventListener('click', async () => {
-    // A variável 'supabaseClient' também vem do auth.js
-    const { error } = await supabaseClient.auth.signOut();
-
-    if (error) {
-        alert("Erro ao fazer logout: " + error.message);
-    } else {
-        alert("Você foi desconectado.");
-        window.location.href = "index.html";
-    }
-});
 
 async function buscarPersonagens() {
     const lista = document.getElementById('personagens-lista');
