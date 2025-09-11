e4cfcc726dc6d60be54021796f47d4b81df4aa5c
gustavoq <Comercial@rede.tecmetro>
2025-09-11 16:40:24 -0300

    Alpha 1.6

---
+++ testesRPG/subirnivel.js
@@ -0,0 +1,9 @@
+
+
+function generica{
+    const nivelv = document.getElementById("nivel")
+    const output = document.getElementById("valores")
+    
+}
+
+    input.addEventListener("valores",generica)
